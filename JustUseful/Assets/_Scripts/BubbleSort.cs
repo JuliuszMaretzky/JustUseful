9298130d21267c53f42f6da689370f05a083d4df
Juliusz Maretzky <kontakt@juliuszmaretzky.pl>
2022-01-05 17:44:59 +0100

    better bubble sort

--- JustUseful/Assets/_Scripts/BubbleSort.cs
+++ JustUseful/Assets/_Scripts/BubbleSort.cs
@@ -8,12 +8,20 @@ public static class BubbleSort
     {
         for(int i=0;i<array.Length;i++)
         {
-            for(int j=0;j<array.Length-1;j++)
+            var isAnyChange = false;
+
+            for (int j=0;j<array.Length-1;j++)
             {
                 if(array[j].CompareTo(array[j+1])>0)
                 {
+                    isAnyChange = true;
                     Swap(array, j, j + 1);
                 }
+            }
+
+            if(!isAnyChange)
+            {
+                break;
             }
         }
     }
